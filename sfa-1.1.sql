ad98b71a7c2f04911e679b91f3e0e0c1cb53bc4e
Psykka <viniciusfabrisilk@gmail.com>
2019-06-21 23:29:37 -0300

    login screen

---
+++ sfa-1.1.sql
@@ -0,0 +1,62 @@
+create database SFA;
+
+use SFA;
+
+create table usuarios(
+	idUser int(10) unsigned not null auto_increment,
+    username varchar(255) not null,
+    passwd varchar(255) default null,
+    primary key (idUser)
+);
+
+create table cargo(
+	idCargo int(10) unsigned not null auto_increment,
+	funcao varchar(255) not null,
+    primary key (idCargo)
+);
+
+create table horario(
+	idHorario int(10) unsigned not null auto_increment,
+    dia varchar(30) not null,
+	horaIni time not null,
+    horaFin time not null,
+    primary key (idHorario)
+);
+
+create table funcionario(
+	idFunc int(10) unsigned not null auto_increment,
+    idHorario int(10) unsigned not null,
+    nome varchar (255) not null,
+	idCargo int(10) unsigned not null,
+    primary key (idFunc),
+    foreign key (idCargo) references cargo(idCargo),
+    foreign key (idHorario) references horario(idHorario)
+);
+
+create table motivo(
+	idMotivo int(10) unsigned not null auto_increment,
+    motivo varchar(255),
+    primary key (idMotivo)
+);
+
+create table condicao(
+	idCondicao int(10) unsigned not null auto_increment,
+    condicao varchar(255) not null,
+    idMotivo int(10) unsigned default null, /* Motivo da falta se o funcionario faltou */
+    foreign key (idMotivo) references motivo(idMotivo),
+    primary key (idCondicao)
+);
+
+create table frequencia(
+	idFrequencia int(10) unsigned not null auto_increment,
+    idFunc int(10) unsigned not null,
+    idCondicao int(10) unsigned not null,
+    dia date not null,
+    FreqIni time not null,
+    FreqFin time not null,
+    foreign key (idFunc) references funcionario(idFunc),
+    foreign key (idCondicao) references condicao(idCondicao),
+    primary key (idFrequencia)
+);
+
+
---
+++ site/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="css/style.css">
+    <title>SFA | Login</title>
+</head>
+<body>
+    <div class="login">
+    <h1>login</h1>
+    <img src="https://via.placeholder.com/250x50">
+        <form action="menu.php" method="post">
+            <input type="text" name="user"placeholder="User">
+            <input type="password" name="pass" placeholder=Password>
+            <input type="submit" value="Login">
+        </form>
+    </div>
+</body>
+</html>
