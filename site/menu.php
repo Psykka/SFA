7c76b3f960ee0df4aa658863ed49d9dc4402eb7a
Psykka <viniciusfabrisilk@gmail.com>
2019-06-26 08:44:43 -0300

    login changes

---
+++ site/menu.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="css/style.css">
+    <title>SFA | Manu</title>
+</head>
+<body>
+    <div class="container">
+        a
+    </div>
+</body>
+</html>
