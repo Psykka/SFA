447243250e3c058d0389e2127b5576ff732774d1
Psykka <viniciusfabrisilk@gmail.com>
2019-08-17 15:22:29 -0300

    Search Engine

--- site/funcionarios.php
+++ site/funcionarios.php
@@ -8,9 +8,38 @@
 </head>
 <body>
     <?php
-    ?>
-    <div class="container">
+        session_start();
 
+        if(!isset($_SESSION['login_user'])){
+            header("location:sessao.php");
+            die();
+        }
+
+        require_once("php/CMS.php");
+        $cms = new CMS();
+        $db = $cms->conectar();
+
+        $query = "SELECT * FROM funcionario";
+
+        $result = mysqli_query($db, $query);
+
+        $rows = Array();
+
+        while($i = mysqli_fetch_assoc($result)){
+            $rows[] = $i;
+        }
+
+    ?>
+
+    <div class="container">
+        <label>Nome:</label><input type="text" id="nome" onkeyup="rowSearch(funcionarios, 'nome', 'search');">
+
+        <div id="search" width="500"></div>
     </div>
+    
+    <script type="text/javascript">
+        const funcionarios = JSON.parse('<?php echo json_encode($rows); ?>');
+    </script>
+    <script src="./js/rowSearch.js"></script>
 </body>
 </html>
---
+++ site/js/rowSearch.js
@@ -0,0 +1,21 @@
+function rowSearch(funcionarios, inputId, divId){
+    let input = document.getElementById(inputId).value.toLowerCase(),
+        query = funcionarios.map( f =>{ return {nome: f.nome, id: f.idFunc} }).filter( n =>{ return n.nome.toLowerCase().includes(input) })
+
+    let result = ""
+
+    if(!query[0]){
+        result = "Não consegui encontrar";
+
+    }else{
+        let list = ""
+
+        query.forEach(funcionario => {
+            list = list + `<tr><td>${ funcionario.id }</td><td>${ funcionario.nome }</td></tr>`;
+        });
+
+        result = `<table>${ list }</table>`
+    }
+
+    return document.getElementById(divId).innerHTML = result;
+}
