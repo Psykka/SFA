ba505f9a21d4c27e52d5f7c925d556a75798c355
Psykka <viniciusfabrisilk@gmail.com>
2019-09-25 01:36:07 -0300

    Concatenando o que alguem colocou fora do lugar

--- site/funcionarios.php
+++ site/funcionarios.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -9,14 +10,16 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.js"></script>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
+    </script>
     <script type="text/javascript">
-        $(document).ready(() =>{
-            $('#editRG').mask('00.000.000-0')
-        })
+    $(document).ready(() => {
+        $('#editRG').mask('00.000.000-0')
+    })
     </script>
     <title>SFA | Funcionarios</title>
 </head>
+
 <body onload="rowSearch(funcionarios, 'nome', 'search');">
     <?php
         session_start();
@@ -46,13 +49,13 @@
             die();
         }
     ?>
-    
+
     <header class="d-flex justify-content-between">
         <div>
             <a href="menu.php">
                 <img src="./assets/back.png">
                 <strong>Voltar</strong>
-        </a>
+            </a>
         </div>
         <div>
             <a href="menu.php?logout=true">
@@ -62,106 +65,124 @@
     </header>
 
     <div class="container">
-    <div class="input-group input-group-sm mb-3">
-        <div class="input-group-prepend">
-            <span class="input-group-text" id="inputGroup-sizing-sm">Nome</span>
+        <div class="cadastro">
+            <h1>Funcionários</h1>
         </div>
-        <input type="text" class="form-control" autocomplete="off" id="nome" onkeyup="rowSearch(funcionarios, 'nome', 'search');">
-    </div>
-        
-    
-
+        <div class="input-group input-group-sm mb-3">
+            <div class="input-group-prepend">
+                <span class="input-group-text" id="inputGroup-sizing-sm">Nome</span>
+            </div>
+            <input type="text" class="form-control" autocomplete="off" id="nome" onkeyup="rowSearch(funcionarios, 'nome', 'search');">
+        </div>
         <div id="search" width="500"></div>
     </div>
-    
+
     <script type="text/javascript">
-        const funcionarios = JSON.parse('<?php echo json_encode($rows); ?>');
+    const funcionarios = JSON.parse('<?php echo json_encode($rows); ?>');
     </script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@8"></script>
     <script type="text/javascript">
-        async function editFunc(id) {
-            let func = funcionarios.map( f =>{ return {nome: f.nome, id: f.idFunc, rg: f.rg} })
-                .filter( n =>{ return n.id == id })
+    async function editFunc(id) {
+        let func = funcionarios.map(f => {
+                return {
+                    nome: f.nome,
+                    id: f.idFunc,
+                    rg: f.rg
+                }
+            })
+            .filter(n => {
+                return n.id == id
+            })
 
-            
-            await Swal.fire({
-                title: `<strong>FUNCIONÁRIO (ID: ${func[0].id})</strong>`,
-                type: 'info',
-                html: `<form action="" method="post">
+
+        await Swal.fire({
+            title: `<strong>FUNCIONÁRIO (ID: ${func[0].id})</strong>`,
+            type: 'info',
+            html: `<form action="" method="post">
                             <p>
                                 Nome:<input class="swal2-input" value="${func[0].nome}" id="editNome"><br />
                                 RG:<input class="swal2-input" value="${func[0].rg}" id="editRG" maxlength="9">
                             </p>
                         </form>`,
-                showCancelButton: true,
-                focusConfirm: true,
-                cancelButtonText: 'CANCELAR',
-                cancelButtonColor: '#d33',
-                confirmButtonText: 'SALVAR',
-                confirmButtonColor: '#FF8300',
-                showLoaderOnConfirm: true,
-                allowOutsideClick: false,
-                preConfirm: () =>{
-                    return $.ajax({
-                            type: "POST",
-                            url: 'php/Update.php',
-                            data: {
-                                nome: document.getElementById('editNome').value,
-                                rg: document.getElementById('editRG').value,
-                                id: func[0].id
-                            },
-                            dataType: 'html'
-                    })
-                },
-            }).then(result => {
-                if(result.value >= 1){
-                    Swal.fire('Sucesso!', 'As alteções foram realizadas.', 'success').then(result =>{
-                        window.location.href = ('funcionarios.php')
-                    })
-                }else{
-                    if(result.value == 0 || result.dismiss === Swal.DismissReason.cancel) return Swal.fire('Cancelado!', 'As alteções não foram realizadas.', 'error')
-                    Swal.fire('Erro!', result.value, 'error');
-                }
-            });
-        }
+            showCancelButton: true,
+            focusConfirm: true,
+            cancelButtonText: 'CANCELAR',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'SALVAR',
+            confirmButtonColor: '#FF8300',
+            showLoaderOnConfirm: true,
+            allowOutsideClick: false,
+            preConfirm: () => {
+                return $.ajax({
+                    type: "POST",
+                    url: 'php/Update.php',
+                    data: {
+                        nome: document.getElementById('editNome').value,
+                        rg: document.getElementById('editRG').value,
+                        id: func[0].id
+                    },
+                    dataType: 'html'
+                })
+            },
+        }).then(result => {
+            if (result.value >= 1) {
+                Swal.fire('Sucesso!', 'As alteções foram realizadas.', 'success').then(result => {
+                    window.location.href = ('funcionarios.php')
+                })
+            } else {
+                if (result.value == 0 || result.dismiss === Swal.DismissReason.cancel) return Swal.fire(
+                    'Cancelado!', 'As alteções não foram realizadas.', 'error')
+                Swal.fire('Erro!', result.value, 'error');
+            }
+        });
+    }
     </script>
     <script type="text/javascript">
-            async function deleteFunc(id){
-                let func = funcionarios.map( f =>{ return {nome: f.nome, id: f.idFunc, rg: f.rg} })
-                            .filter( n =>{ return n.id == id })
-
-                await Swal.fire({
-                title: `<strong>Deseja deletar o funcinario\n${func[0].nome}</strong>`,
-                type: 'info',
-                showCancelButton: true,
-                focusConfirm: true,
-                cancelButtonText: 'CANCELAR',
-                cancelButtonColor: '#d33',
-                confirmButtonText: 'SIM',
-                confirmButtonColor: '#FF8300',
-                showLoaderOnConfirm: true,
-                allowOutsideClick: false,
-                preConfirm: () =>{
-                    return $.ajax({
-                            type: "POST",
-                            url: 'php/Delete.php',
-                            data: {
-                                id: func[0].id
-                            },
-                            dataType: 'html'
-                    })
-                },
-            }).then(result => {
-                if(result.value >= 1){
-                    Swal.fire('Sucesso!', 'O funcionário foi deletado com sucesso.', 'success').then(result =>{
-                        window.location.href = ('funcionarios.php')
-                    })
-                }else{
-                    if(result.value == 0 || result.dismiss === Swal.DismissReason.cancel) return Swal.fire('Cancelado!', 'As alteções não foram realizadas.', 'error')
-                    Swal.fire('Erro!', result.value, 'error');
+    async function deleteFunc(id) {
+        let func = funcionarios.map(f => {
+                return {
+                    nome: f.nome,
+                    id: f.idFunc,
+                    rg: f.rg
                 }
-            });
-        }
+            })
+            .filter(n => {
+                return n.id == id
+            })
+
+        await Swal.fire({
+            title: `<strong>Deseja deletar o funcinario\n${func[0].nome}</strong>`,
+            type: 'info',
+            showCancelButton: true,
+            focusConfirm: true,
+            cancelButtonText: 'CANCELAR',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'SIM',
+            confirmButtonColor: '#FF8300',
+            showLoaderOnConfirm: true,
+            allowOutsideClick: false,
+            preConfirm: () => {
+                return $.ajax({
+                    type: "POST",
+                    url: 'php/Delete.php',
+                    data: {
+                        id: func[0].id
+                    },
+                    dataType: 'html'
+                })
+            },
+        }).then(result => {
+            if (result.value >= 1) {
+                Swal.fire('Sucesso!', 'O funcionário foi deletado com sucesso.', 'success').then(result => {
+                    window.location.href = ('funcionarios.php')
+                })
+            } else {
+                if (result.value == 0 || result.dismiss === Swal.DismissReason.cancel) return Swal.fire(
+                    'Cancelado!', 'As alteções não foram realizadas.', 'error')
+                Swal.fire('Erro!', result.value, 'error');
+            }
+        });
+    }
     </script>
     <?php       
         if(isset($_GET['funcId'])){
@@ -174,4 +195,5 @@
     ?>
     <script src="./js/rowSearch.js"></script>
 </body>
+
 </html>
