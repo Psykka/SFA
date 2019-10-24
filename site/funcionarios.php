bfc94547891646d5d1bd706295a5db774f9f464d
Psykka <viniciusfabrisilk@gmail.com>
2019-10-24 01:41:51 -0300

    Ops

--- site/funcionarios.php
+++ site/funcionarios.php
@@ -5,7 +5,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="sweetalert2.min.css">
     <link rel="shortcut icon" href="assets/Logo2.png" type="image/x-icon">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.js"></script>
