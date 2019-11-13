aabc41af851d612f10229f1e2b0c1029783d0661
Psykka <viniciusfabrisilk@gmail.com>
2019-11-13 14:38:35 -0300

    mais toques finais 🎉

--- site/cadastro.php
+++ site/cadastro.php
@@ -8,47 +8,45 @@
     <link rel="shortcut icon" href="assets/Logo2.png" type="image/x-icon">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.js"></script>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
-        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
     </script>
     <script type="text/javascript">
-    $(document).ready(() => {
-        $('#RG').mask('00.000.000-0')
-    })
+        $(document).ready(() => {
+            $('#RG').mask('00.000.000-0')
+        })
     </script>
     <title>SFA | Cadastro</title>
 </head>
 
 <body>
     <?php
-        session_start();
+    session_start();
 
-        require_once("php/CMS.php");
-        $cms = new CMS();
-        $db = $cms->conectar();
+    require_once("php/CMS.php");
+    $cms = new CMS();
+    $db = $cms->conectar();
 
-        $query = "SELECT * FROM cargo";
+    $query = "SELECT * FROM cargo";
 
-        $result = mysqli_query($db, $query);
+    $result = mysqli_query($db, $query);
 
-        $rows = Array();
+    $rows = array();
 
-        while($i = mysqli_fetch_assoc($result)){
-            $rows[] = $i;
-        }
+    while ($i = mysqli_fetch_assoc($result)) {
+        $rows[] = $i;
+    }
 
-        if(!isset($_SESSION['login_user'])){
-            header("location:sessao.php");
-            die();
-        }
+    if (!isset($_SESSION['login_user'])) {
+        header("location:sessao.php");
+        die();
+    }
 
-        if(isset($_GET['logout']) == true){
-            session_destroy();
-            header("Location: index.php");
-            die();
-        }
+    if (isset($_GET['logout']) == true) {
+        session_destroy();
+        header("Location: index.php");
+        die();
+    }
     ?>
 
     <header class="d-flex justify-content-between">
@@ -84,12 +82,12 @@
                 <select name="cargo" form="form2" class="custom-select" id="cargo" required>
                     <option value="" selected>Escolha um cargo...</option>
                     <?php
-                        foreach ($rows as $value) {
-                            $id = $value['idCargo'];
-                            $nome = $value['funcao'];
+                    foreach ($rows as $value) {
+                        $id = $value['idCargo'];
+                        $nome = $value['funcao'];
 
-                            echo "<option value='$id'>$nome</option>";
-                        }
+                        echo "<option value='$id'>$nome</option>";
+                    }
                     ?>
                 </select>
                 <form method="post" id="form2">
@@ -126,7 +124,7 @@
         async function insert(inset) {
             url = '';
             data = {};
-            switch (inset){
+            switch (inset) {
                 case 'motivo':
                     url = 'php/CadastroMotivo.php'
                     data = {
@@ -167,14 +165,17 @@
                         data: data,
                         dataType: 'html'
                     })
-                },
+                }
             }).then(result => {
                 if (result.value >= 1) {
                     Swal.fire({
                         type: 'success',
                         title: 'Cadastrado com sucesso!',
                         showConfirmButton: false,
-                        timer: 1500
+                        timer: 1500,
+                        onClose: () => {
+                            window.location.reload();
+                        }
                     })
                 } else {
                     if (result.value == 0 || result.dismiss === Swal.DismissReason.cancel) return Swal.fire(
@@ -183,7 +184,6 @@
                 }
             });
         }
-
     </script>
 </body>
 
