a8d296ab52272a79235ec3262cf0c678edf3fdd2
vinicius augusto alves <viniciusfabrisilk@gmail.com>
2019-11-18 02:23:00 -0300

    Revert "oof"
    This reverts commit ce840df4b1329a94db03fecdaff9a5f8d6ee6312.

--- site/js/rowSearch.js
+++ site/js/rowSearch.js
@@ -1,26 +1,33 @@
 function rowSearch(funcionarios, inputId, divId){
     let input = document.getElementById(inputId).value.toLowerCase(),
-        query = funcionarios.map(({nome, idFunc, rg}) => ({nome, rg, id: f.idFunc })).filter(({nome}) => nome.toLowerCase().includes(input));
+        query = funcionarios.map( f =>{ return {nome: f.nome, id: f.idFunc, rg: f.rg} }).filter( n =>{ return n.nome.toLowerCase().includes(input) })
 
-    let result = '';
+    let result = ""
 
-    if(!query[0]) return "Não consegui encontrar";
-    let list = ''
+    if(!query[0]){
+        result = "Não consegui encontrar";
 
-    query.forEach(funcionario => {
-        list = list + `<tr>
-                            <td>${ funcionario.id }</td>
-                            <td>${ funcionario.nome }</td>
-                            <td>
-                                <a href="funcionarios.php?funcId=${ funcionario.id }">
-                                    <img src="./assets/edit.png">
-                                </a>
-                                <a href="funcionarios.php?deleteId=${ funcionario.id }">
+    }else{
+        let list = ""
+
+        query.forEach(funcionario => {
+            list = list + `<tr>
+                                <td>${ funcionario.id }</td>
+                                <td>${ funcionario.nome }</td>
+                                <td>
+                                    <a href="funcionarios.php?funcId=${ funcionario.id }">
+                                        <img src="./assets/edit.png">
+                                    </a>
+                                    <a href="funcionarios.php?deleteId=${ funcionario.id }">
                                     <img src="./assets/delete.png">
-                                </a>
-                            </td>
-                        </tr>`;
+                                    </a>
+                                </td>
+                            </tr>`;
         });
-    result = `<table>${ list }</table>`
+
+        result = `<table>${ list }</table>`
+        
+    }
+
     return document.getElementById(divId).innerHTML = result;
 }
